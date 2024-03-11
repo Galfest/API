acedcdfaffc8fcbe3a88ba6329300446efb0d605
Galfest <abramyants90@mail.ru>
2024-03-11 15:38:17 +0300

    HW1

--- src/main/java/abramyants/Task3.java
+++ src/main/java/abramyants/Task3.java
@@ -6,6 +6,7 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.println("Введите первое число:");
         double num1 = scanner.nextDouble();
 
